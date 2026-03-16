ffe5ea25d1a76c0bdf45d3d678adea121daa38c8
ncor81 <natnat81@gmail.com>
2026-03-16 18:32:47 -0400

    Added gameloop and player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants  import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants  import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,8 +9,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         log_state()
@@ -17,7 +26,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # player.draw(screen)
+        # player.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+
+        dt = (clock.tick(60)) / 1000
+
 
 if __name__ == "__main__":
     main()
