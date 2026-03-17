a1f5e60a5d4dde28c3440fc557a15e61f82d51c9
ncor81 <natnat81@gmail.com>
2026-03-16 20:02:32 -0400

    added collisions and game exit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import pygame
+import pygame, sys
 from constants  import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from logger import log_event
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,7 +17,11 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -26,11 +33,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        # player.draw(screen)
-        # player.update(dt)
         for draw in drawable:
             draw.draw(screen)
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print(f"Game over!")
+                sys.exit()
         pygame.display.flip()
 
         dt = (clock.tick(60)) / 1000
